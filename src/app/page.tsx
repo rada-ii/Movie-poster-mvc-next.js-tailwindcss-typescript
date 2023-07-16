78d66c680b55fa06cff27aa83c7e1b4bdb22c80b
rada-i <neposebanmail@gmail.com>
2023-07-16 22:28:46 +0200

    corrections and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import MoviePoster from "@/views/MoviePoster";
 const MoviePage: React.FC = () => {
   return (
     <div>
-      <h1 className="text-3xl text-orange-500 text-center m-auto p-6">
+      <h1 className="text-3xl text-orange-700 text-center m-auto p-6">
         Movie App
       </h1>
       <MoviePoster />
